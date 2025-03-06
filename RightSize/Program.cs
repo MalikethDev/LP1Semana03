af227cc5a2e0265359f31dbb5a7e557a47d36dde
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-06 15:49:14 +0000

    Using foreach if the lenght of the arguments are superior to 3 print it

---
+++ RightSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Using foreach
+            // Print the arguments in cmd with lenght superiror to 3
+            foreach (string s in args)
+            {
+                if (s.Length > 3)
+                {
+                    Console.WriteLine(s);
+                }
+            } 
+        }
+    }
+}
