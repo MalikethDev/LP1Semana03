3d48a833f4676e4fbe99e58d75b685f7f3bc66bb
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-06 16:28:39 +0000

    Added ArrayMul

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
