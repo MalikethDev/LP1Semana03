becc6f94142854a146833538774db5908efe6b2d
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-06 16:02:51 +0000

    Ask the user for a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the user for a string
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+        }
+    }
+}
