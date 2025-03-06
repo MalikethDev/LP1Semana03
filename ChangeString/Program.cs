d482ac35d4921661ab96fea13a9437d45cc3bed8
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-06 16:06:25 +0000

    Ask the user for a character

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -9,6 +9,11 @@ namespace ChangeString
             // Ask the user for a string
             Console.WriteLine("Enter a string:");
             string input = Console.ReadLine();
+
+            // Ask the user for a character
+            Console.WriteLine("Enter a character:");
+            char character = Console.ReadKey().KeyChar;
+            Console.WriteLine();
         }
     }
 }
