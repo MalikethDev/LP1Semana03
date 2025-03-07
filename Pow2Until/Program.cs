56a6a8db86b6a9952249f734c39fad1d55c33896
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-07 12:12:35 +0000

    Called method from main

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -4,6 +4,11 @@ namespace Pow2Until
 {
     public class Program
     {
+        // Call method from main
+        private static void Main(string[] args)
+        {
+            Powersof2Until5();
+        }
         private static void Powersof2Until5()
         {
             for (int i = 1; i <= (1 << 5); i = i << 1)
